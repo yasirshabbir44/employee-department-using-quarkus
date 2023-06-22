aa8a638ed93462205c8b6fec15d333241d14bdb1
Yasir Shabbir <yasirshabbir44@gmail.com>
2023-06-22 21:07:52 +0400

    high salary employee salry

--- src/main/java/com/damac/DepartmentResource.java
+++ src/main/java/com/damac/DepartmentResource.java
@@ -1,6 +1,9 @@
 package com.damac;
 
 
+import com.damac.service.MapperService;
+import dto.DepartmentDTO;
+import dto.EmployeeDTO;
 import jakarta.transaction.Transactional;
 import model.Department;
 
@@ -9,8 +12,11 @@ import jakarta.ws.rs.core.MediaType;
 
 
 import jakarta.ws.rs.core.Response;
+import model.Employee;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/departments")
 @Produces(MediaType.APPLICATION_JSON)
@@ -27,6 +33,38 @@ public class DepartmentResource {
     public Department getDepartmentById(@PathParam("id") Long id) {
         // Implement logic to fetch a department by ID
         return Department.findById(id);
+    }
+
+
+    @GET
+    @Path("/max-salary-employees")
+    public Response getDepartmentsWithMaxSalaryEmployees() {
+        List<Department> departments = Department.listAll();
+
+        List<DepartmentDTO> departmentDTOs = new ArrayList<>();
+
+        for (Department department : departments) {
+            double maxSalary = department.getEmployees()
+                    .stream()
+                    .mapToDouble(Employee::getSalary)
+                    .max()
+                    .orElse(0.0);
+
+            List<EmployeeDTO> employeesWithMaxSalary = department.getEmployees()
+                    .stream()
+                    .filter(employee -> employee.getSalary() == maxSalary)
+                    .map(MapperService::convertToEmployeeDTO)
+                    .collect(Collectors.toList());
+
+            DepartmentDTO departmentDTO = new DepartmentDTO();
+            departmentDTO.setId(department.id);
+            departmentDTO.setName(department.getName());
+            departmentDTO.setEmployees(employeesWithMaxSalary);
+
+            departmentDTOs.add(departmentDTO);
+        }
+
+        return Response.ok(departmentDTOs).build();
     }
 
     @POST
